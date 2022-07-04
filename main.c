16819440dc77477efb49ac539bc09a28fe2467e5
Predo <preston.dresser16@gmail.ca>
2022-07-03 20:59:27 -0400

    add logging control for parser

--- main.c
+++ main.c
@@ -50,6 +50,8 @@ void app_main(void){
     esp_log_level_set("httpd_sess", ESP_LOG_DEBUG);
     esp_log_level_set("httpd_uri", ESP_LOG_DEBUG);
     //esp_log_level_set("httpd_parse", ESP_LOG_DEBUG);
+    //esp_log_level_set("httpd_parser", ESP_LOG_DEBUG);
+
 
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
