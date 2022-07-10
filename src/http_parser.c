4ed98ebad87a14d973dca9736ef120864434d736
Predo <preston.dresser16@gmail.ca>
2022-07-10 19:22:19 -0400

    cleaned up parser

--- src/http_parser.c
+++ src/http_parser.c
@@ -33,120 +33,7 @@
 
 static const char *TAG = "httpd_parser";
 
-#ifndef ULLONG_MAX
-# define ULLONG_MAX ((uint64_t) -1) /* 2^64-1 */
-#endif
-
-#ifndef MIN
-# define MIN(a,b) ((a) < (b) ? (a) : (b))
-#endif
-
-#ifndef ARRAY_SIZE
-# define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
-#endif
-
-#ifndef BIT_AT
-# define BIT_AT(a, i)                                                \
-  (!!((unsigned int) (a)[(unsigned int) (i) >> 3] &                  \
-   (1 << ((unsigned int) (i) & 7))))
-#endif
-
-#ifndef ELEM_AT
-# define ELEM_AT(a, i, v) ((unsigned int) (i) < ARRAY_SIZE(a) ? (a)[(i)] : (v))
-#endif
-
-#define SET_ERRNO(e)                                                 \
-do {                                                                 \
-  parser->http_errno = (e);                                          \
-} while(0)
-
-#define CURRENT_STATE() p_state
-#define UPDATE_STATE(V) p_state = (enum state) (V);
-#define RETURN(V)                                                    \
-do {                                                                 \
-  parser->state = CURRENT_STATE();                                   \
-  return (V);                                                        \
-} while (0);
-#define REEXECUTE()                                                  \
-  goto reexecute;                                                    \
-
-
-#ifdef __GNUC__
-# define LIKELY(X) __builtin_expect(!!(X), 1)
-# define UNLIKELY(X) __builtin_expect(!!(X), 0)
-#else
-# define LIKELY(X) (X)
-# define UNLIKELY(X) (X)
-#endif
-
-
-/* Run the notify callback FOR, returning ER if it fails */
-#define CALLBACK_NOTIFY_(FOR, ER)                                    \
-do {                                                                 \
-  ESP_LOGD(TAG, LOG_FMT("CALLBACK NOTIFY"));                         \
-  assert(HTTP_PARSER_ERRNO(parser) == HPE_OK);                       \
-                                                                     \
-  if (LIKELY(settings->on_##FOR)) {                                  \
-    parser->state = CURRENT_STATE();                                 \
-    if (UNLIKELY(0 != settings->on_##FOR(parser))) {                 \
-      SET_ERRNO(HPE_CB_##FOR);                                       \
-    }                                                                \
-    UPDATE_STATE(parser->state);                                     \
-                                                                     \
-    /* We either errored above or got paused; get out */             \
-    if (UNLIKELY(HTTP_PARSER_ERRNO(parser) != HPE_OK)) {             \
-      return (ER);                                                   \
-    }                                                                \
-  }                                                                  \
-} while (0)
-
-/* Run the notify callback FOR and consume the current byte */
-#define CALLBACK_NOTIFY(FOR)            CALLBACK_NOTIFY_(FOR, p - data + 1)
-
-/* Run the notify callback FOR and don't consume the current byte */
-#define CALLBACK_NOTIFY_NOADVANCE(FOR)  CALLBACK_NOTIFY_(FOR, p - data)
-
-/* Set the mark FOR; non-destructive if mark is already set */
-#define MARK(FOR)                                                    \
-do {                                                                 \
-  if (!FOR##_mark) {                                                 \
-    FOR##_mark = p;                                                  \
-  }                                                                  \
-} while (0)
-
-/* Don't allow the total size of the HTTP headers (including the status
- * line) to exceed HTTP_MAX_HEADER_SIZE.  This check is here to protect
- * embedders against denial-of-service attacks where the attacker feeds
- * us a never-ending header that the embedder keeps buffering.
- *
- * This check is arguably the responsibility of embedders but we're doing
- * it on the embedder's behalf because most won't bother and this way we
- * make the web a little safer.  HTTP_MAX_HEADER_SIZE is still far bigger
- * than any reasonable request or response so this should never affect
- * day-to-day operation.
- */
-#define COUNT_HEADER_SIZE(V)                                         \
-do {                                                                 \
-  parser->nread += (V);                                              \
-  if (UNLIKELY(parser->nread > (HTTP_MAX_HEADER_SIZE))) {            \
-    SET_ERRNO(HPE_HEADER_OVERFLOW);                                  \
-    goto error;                                                      \
-  }                                                                  \
-} while (0)
-
-
-#define PROXY_CONNECTION "proxy-connection"
-#define CONNECTION "connection"
-#define CONTENT_LENGTH "content-length"
-#define TRANSFER_ENCODING "transfer-encoding"
-#define UPGRADE "upgrade"
-#define CHUNKED "chunked"
-#define KEEP_ALIVE "keep-alive"
-#define CLOSE "close"
-
-
 static char * valid = "!#$%%&\'*+-.0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ^_`abcdefghijklmnopqrstuvwxyz";
-
 
 static char * valid_url = "!\"$%%&\'()*+,-./0123456789:;<=>1@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
 
@@ -247,21 +134,27 @@ size_t http_parser_execute (http_parser *parser, const http_parser_settings *set
     if (p_state == s_start_req){
       return 0;
     }else{
-      SET_ERRNO(HPE_INVALID_EOF_STATE);
+      parser->http_errno = HPE_INVALID_EOF_STATE;
       return 1;
     }
   }
 
   //these marks are for callback functions
-  if (CURRENT_STATE() == s_header_field) header_field_mark = data;
-  if (CURRENT_STATE() == s_header_value) header_value_mark = data;
-  if (CURRENT_STATE() == s_req_path) url_mark = data;
+  if (p_state == s_header_field) header_field_mark = data;
+  if (p_state == s_header_value) header_value_mark = data;
+  if (p_state == s_req_path) url_mark = data;
 
   ESP_LOGD(TAG, LOG_FMT("data:\n%s"), data);
   
   for (p=data; p != data + len; p++) {
     ch = *p;
-    if (PARSING_HEADER(CURRENT_STATE())) COUNT_HEADER_SIZE(1);
+    if (p_state <= s_headers_done){
+      parser->nread += 1;
+      if (parser->nread > (HTTP_MAX_HEADER_SIZE)) {
+        parser->http_errno = HPE_HEADER_OVERFLOW;
+        goto error;
+      }
+    }
 
 reexecute:
     switch (p_state) {
@@ -518,7 +411,18 @@ reexecute:
 
         }
 
-        COUNT_HEADER_SIZE(p - start);
+        parser->nread += p - start;
+        if (parser->nread > (HTTP_MAX_HEADER_SIZE)) {
+          parser->http_errno = HPE_HEADER_OVERFLOW;
+          goto error;
+        }
+        if (p_state <= s_headers_done){
+          parser->nread += p - start;
+          if (parser->nread > (HTTP_MAX_HEADER_SIZE)) {
+            parser->http_errno = HPE_HEADER_OVERFLOW;
+            goto error;
+          }
+        }
 
         if (p == data + len) {
           --p;
@@ -595,7 +499,11 @@ reexecute:
             char nl = '\n';
             strncat(full_req, &nl, 1);
             p_state = (enum state) s_header_almost_done;
-            COUNT_HEADER_SIZE(p - start);
+            parser->nread += p - start;
+            if (parser->nread > (HTTP_MAX_HEADER_SIZE)) {
+              parser->http_errno = HPE_HEADER_OVERFLOW;
+              goto error;
+            }
             if (0 != settings->on_header_value(parser, header_value_mark, p - header_value_mark)) parser->http_errno = HPE_CB_header_value;
             if (parser->http_errno != HPE_OK) return (p - data);
             p_state = (enum state) parser->state;
@@ -625,7 +533,11 @@ reexecute:
           --p;
         }
 
-        COUNT_HEADER_SIZE(p - start);
+        parser->nread += p - start;
+        if (parser->nread > (HTTP_MAX_HEADER_SIZE)) {
+          parser->http_errno = HPE_HEADER_OVERFLOW;
+          goto error;
+        }
 
         if (p == data + len)
           --p;
@@ -689,13 +601,15 @@ reexecute:
             case 0:
               break;
             default:
-              SET_ERRNO(HPE_CB_headers_complete);
-              RETURN(p - data); /* Error */
+              parser->http_errno = HPE_CB_headers_complete;
+              parser->state = p_state;
+              return (p - data);
           }
         }
 
         if (HTTP_PARSER_ERRNO(parser) != HPE_OK) {
-          RETURN(p - data);
+          parser->state = p_state;
+          return (p - data);
         }
 
         goto reexecute;
@@ -705,9 +619,9 @@ reexecute:
         parser_state("s_headers_done", ch);
         ESP_LOGI(TAG, LOG_FMT("parsed request of length %d\n\n%s"), overall_len, full_req);
         overall_len = 0;
-        if (ch != '\n'){ SET_ERRNO(HPE_STRICT); goto error;}
-        parser->state = CURRENT_STATE();
-        if (0 != settings->on_message_complete(parser)) SET_ERRNO(HPE_CB_message_complete);
+        if (ch != '\n'){ parser->http_errno = HPE_STRICT; goto error;}
+        parser->state = p_state;
+        if (0 != settings->on_message_complete(parser)) parser->http_errno = HPE_CB_message_complete;
         p_state = (enum state) (parser->state);
         if (HTTP_PARSER_ERRNO(parser) != HPE_OK) return (p - data + 1);
         parser->state = p_state;
@@ -717,7 +631,7 @@ reexecute:
       default:
       {
         assert(0 && "unhandled state");
-        SET_ERRNO(HPE_INVALID_INTERNAL_STATE);
+        parser->http_errno = HPE_INVALID_INTERNAL_STATE;
         goto error;
       }
     }
@@ -727,7 +641,7 @@ reexecute:
 
 error:
   if (HTTP_PARSER_ERRNO(parser) == HPE_OK) {
-    SET_ERRNO(HPE_UNKNOWN);
+    parser->http_errno = HPE_UNKNOWN;
   }
   parser->state = p_state;
   return (p - data);
@@ -799,7 +713,7 @@ void http_parser_pause(http_parser *parser, int paused) {
    */
   if (HTTP_PARSER_ERRNO(parser) == HPE_OK ||
       HTTP_PARSER_ERRNO(parser) == HPE_PAUSED) {
-    SET_ERRNO((paused) ? HPE_PAUSED : HPE_OK);
+    parser->http_errno = (paused) ? HPE_PAUSED : HPE_OK;
   } else {
     assert(0 && "Attempting to pause parser in error state");
   }
