f3b530448193de5953e0c6bab152ba78af6efbcc
Predo <preston.dresser16@gmail.ca>
2022-07-17 21:48:41 -0400

    change stopper name in header file

--- inc/PredoHttpServer.h
+++ inc/PredoHttpServer.h
@@ -1308,7 +1308,7 @@ typedef void (*httpd_work_fn_t)(void *arg);
  */
 esp_err_t httpd_queue_work(httpd_handle_t handle, httpd_work_fn_t work, void *arg);
 
-esp_err_t http_stopper(httpd_handle_t handle);
+esp_err_t http_queue_shutdown(httpd_handle_t handle);
 
 /** End of Group Work Queue
  * @}
